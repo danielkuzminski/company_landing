3bb541a9e2b5d08e00fa4b549f41c42786c4b544
danielkuzminski <dankuzmina@gmail.com>
2022-07-07 19:51:34 +0200

    contact page work in progress

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,9 +2,36 @@ import React from 'react'
 import './Contact.css'
 
 export default function Contact() {
+  // logic to send data to database
+
   return (
-    <div>
-      <h2>contact</h2>
+    <div className='contact-container'>
+      <div className="contact-data">
+        <p>Skontaktuj sie z nami</p><p> pod numerem telefonu</p><a href='tel:+48 123 456 789'> <i class="fa-solid fa-phone"></i> +48 123 456 789</a>
+        <p>Lub napisz</p>
+        <p><i class="fa-solid fa-arrow-down"></i></p>
+      </div>
+
+      <form className='contact-form'>
+        <h1 className='contact-header'>Skontaktuj się z nami</h1>
+        <label className='form-label'>
+          <span>Temat: </span>
+          <input type="text" required/>
+        </label>
+        <label className='form-label'>
+          <span>Treść wiadomości: </span>
+          <textarea required></textarea>
+        </label>
+        <label className='form-label'>
+          <span>Numer telefonu: </span>
+          <input type="number" required/>
+        </label>
+        <label className='form-label'>
+          <span>Adres e-mail: </span>
+          <input type="email" required/>
+        </label>
+        <button type='submit'>Prześlij</button>
+      </form>
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ export default function Home() {
         <h1 className='home-company-name'>Nazwa Firmy</h1>
         <h3 className='home-company-motto'>"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus, est."</h3>
         <p className='home-company-about'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam nihil sint deserunt unde odit aspernatur recusandae deleniti voluptate, beatae, quia quod cupiditate distinctio enim repellat velit praesentium quibusdam obcaecati consequatur asperiores architecto et. In dignissimos harum eaque commodi earum assumenda iure molestiae quisquam. Maiores corporis vitae ipsam ratione natus reprehenderit.</p>
-        <div>
+        <div style={{zIndex: "2"}}>
           <Link className='home-show-more' to='/offer'>Zobacz więcej <i class="fa-solid fa-arrow-right"></i></Link>
           <Link to='/contact' className='home-show-more'><i class="fa-solid fa-phone"></i> Skontaktuj sie z nami</Link>
         </div>
