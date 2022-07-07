ecbefe863760fd84e7179ff6d4b112e77b2e1f53
danielkuzminski <dankuzmina@gmail.com>
2022-07-07 18:49:15 +0200

    homepage final touch

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,10 @@ export default function Home() {
         <h1 className='home-company-name'>Nazwa Firmy</h1>
         <h3 className='home-company-motto'>"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus, est."</h3>
         <p className='home-company-about'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam nihil sint deserunt unde odit aspernatur recusandae deleniti voluptate, beatae, quia quod cupiditate distinctio enim repellat velit praesentium quibusdam obcaecati consequatur asperiores architecto et. In dignissimos harum eaque commodi earum assumenda iure molestiae quisquam. Maiores corporis vitae ipsam ratione natus reprehenderit.</p>
-        <Link className='home-show-more' to='/offer'>Zobacz więcej</Link>
+        <div>
+          <Link className='home-show-more' to='/offer'>Zobacz więcej <i class="fa-solid fa-arrow-right"></i></Link>
+          <Link to='/contact' className='home-show-more'><i class="fa-solid fa-phone"></i> Skontaktuj sie z nami</Link>
+        </div>
 <div className="overlay"></div>
     </div>
   )
